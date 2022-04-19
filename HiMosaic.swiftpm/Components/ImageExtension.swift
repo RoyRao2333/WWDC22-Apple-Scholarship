a055aaa5079222d7141e5e1f50cb165e8b671c95
Roy Rao <royrao2333@gmail.com>
2022-04-20 00:06:36 +0800

    Adding vision apis

---
+++ HiMosaic.swiftpm/Components/ImageExtension.swift
@@ -0,0 +1,40 @@
+//
+//  ImageExtension.swift
+//  
+//
+//  Created by Roy Rao on 2022/4/19.
+//
+
+import UIKit
+
+extension CGImagePropertyOrientation {
+    init(_ uiOrientation: UIImage.Orientation) {
+        switch uiOrientation {
+            case .up: self = .up
+            case .upMirrored: self = .upMirrored
+            case .down: self = .down
+            case .downMirrored: self = .downMirrored
+            case .left: self = .left
+            case .leftMirrored: self = .leftMirrored
+            case .right: self = .right
+            case .rightMirrored: self = .rightMirrored
+            @unknown default: fatalError("Uncovered cases")
+        }
+    }
+}
+
+extension UIImage.Orientation {
+    init(_ cgOrientation: UIImage.Orientation) {
+        switch cgOrientation {
+            case .up: self = .up
+            case .upMirrored: self = .upMirrored
+            case .down: self = .down
+            case .downMirrored: self = .downMirrored
+            case .left: self = .left
+            case .leftMirrored: self = .leftMirrored
+            case .right: self = .right
+            case .rightMirrored: self = .rightMirrored
+            @unknown default: fatalError("Uncovered cases")
+        }
+    }
+}
--- HiMosaic.swiftpm/ViewModel/HMService.swift
+++ HiMosaic.swiftpm/ViewModel/HMService.swift
@@ -13,12 +13,6 @@ class HMService: ObservableObject {
     
     @Published var textItems: [TextItem] = []
     
-    private lazy var textRequest: VNDetectTextRectanglesRequest = {
-        let request = VNDetectTextRectanglesRequest(completionHandler: onDetectedText)
-        request.reportCharacterBoxes = true
-        return request
-    }()
-    
     private init() {}
 }
 
@@ -26,15 +20,20 @@ class HMService: ObservableObject {
 extension HMService {
     
     func recognizeText(in uiImage: UIImage?) {
-        guard let cgImage = uiImage?.cgImage else { return }
+        guard
+            let cgImage = uiImage?.cgImage,
+            let orientation = uiImage?.imageOrientation
+        else { return }
         
-        let handler = VNImageRequestHandler(cgImage: cgImage)
+        let handler = VNImageRequestHandler(cgImage: cgImage, orientation: .init(orientation))
+        let textRequest = VNDetectTextRectanglesRequest(completionHandler: onDetectedText)
+        textRequest.reportCharacterBoxes = true
         
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
             guard let weakSelf = self else { return }
             
             do {
-                try handler.perform([weakSelf.textRequest])
+                try handler.perform([textRequest])
             } catch {
                 weakSelf.textItems = []
                 print("DEBUG: Recognizing text failed with error: \(error)")
@@ -48,17 +47,19 @@ extension HMService {
     
     private func onDetectedText(request: VNRequest?, error: Error?) {
         guard
-            let observations = request?.results as? [VNRecognizedTextObservation],
+            let observations = request?.results as? [VNTextObservation],
             error == nil
         else { return }
         
-        let items = observations.compactMap { obs -> TextItem? in
-            if let str = obs.topCandidates(1).first?.string {
-                return TextItem(text: str)
-            }
-            return nil
-        }
+//        let items = observations.compactMap { obs -> TextItem? in
+//
+//            if let str = obs.topCandidates(1).first?.string {
+//                return TextItem(text: str)
+//            }
+//            return nil
+//        }
+        print("Observations are \(observations)")
         
-        textItems = items
+//        textItems = items
     }
 }
