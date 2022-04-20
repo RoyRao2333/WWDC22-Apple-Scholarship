20105f6c3f030c63bbc6287e6c52ed65c45dd8e1
Roy Rao <royrao2333@gmail.com>
2022-04-20 23:00:16 +0800

    Pending: 1. Add more regex 2. Improve mosaic effect

--- HiMosaic.swiftpm/View/ContentView.swift
+++ HiMosaic.swiftpm/View/ContentView.swift
@@ -1,143 +1,39 @@
 import SwiftUI
-import Vision
 
 struct ContentView: View {
-    @ObservedObject private var service: HMService = .shared
-    @State private var bName = false
-    @State private var bNumber = false
-    @State private var bPhoneNumber = false
-    @State private var bEmail = false
-    @State private var bUrl = false
-    @State private var bCreditCardNumber = false
     
     var body: some View {
         NavigationView {
-            ZStack {
-                VStack {
-                    Image("fake_info")
-                        .resizable()
-                        .scaledToFit()
-                        .overlay {
-                            GeometryReader { geo in
-                                ForEach($service.textItems, id: \.self) { $item in
-                                    Rectangle()
-                                        .path(in: VNImageRectForNormalizedRect(
-                                            item.normalizedRect,
-                                            Int(geo.size.width),
-                                            Int(geo.size.height)
-                                        ))
-                                        .fill(item.validated ? Color.gray : .black.opacity(0.0001))
-                                        .onTapGesture {
-                                            item.validated.toggle()
-                                        }
-                                }
-                            }
-                        }
+            VStack(spacing: 20) {
+                NavigationLink {
+                    MosaicOperationView()
+                } label: {
+                    HStack {
+                        Image(systemName: "photo.fill")
+                        
+                        Text("Select a sample")
+                    }
+                    .padding(20)
+                    .background(Color.blue.opacity(0.2), in: RoundedRectangle(cornerRadius: 10))
+                }
+                
+                Divider()
+                    .frame(width: 300)
+                
+                NavigationLink {
                     
-                    VStack(alignment: .leading, spacing: 10) {
-                        Toggle("Names", isOn: $bName)
-                            .toggleStyle(CheckboxToggleStyle(style: .square))
-                            .foregroundColor(.blue)
+                } label: {
+                    HStack {
+                        Image(systemName: "photo.fill.on.rectangle.fill")
                         
-                        Toggle("Numbers", isOn: $bNumber)
-                            .toggleStyle(CheckboxToggleStyle(style: .square))
-                            .foregroundColor(.blue)
-                        
-                        Toggle("Phone Numbers", isOn: $bPhoneNumber)
-                            .toggleStyle(CheckboxToggleStyle(style: .square))
-                            .foregroundColor(.blue)
-                        
-                        Toggle("Emails", isOn: $bEmail)
-                            .toggleStyle(CheckboxToggleStyle(style: .square))
-                            .foregroundColor(.blue)
-                        
-                        Toggle("URLs", isOn: $bUrl)
-                            .toggleStyle(CheckboxToggleStyle(style: .square))
-                            .foregroundColor(.blue)
-                        
-                        Toggle("Credit Card Numbers", isOn: $bCreditCardNumber)
-                            .toggleStyle(CheckboxToggleStyle(style: .square))
-                            .foregroundColor(.blue)
+                        Text("Choose from Photo Library")
                     }
-                }
-                
-                if service.textItems.isEmpty {
-                    ProgressView()
-                        .progressViewStyle(.circular)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                        .background(.ultraThinMaterial)
+                    .padding(20)
+                    .background(Color.blue.opacity(0.2), in: RoundedRectangle(cornerRadius: 10))
                 }
             }
-            .navigationTitle("")
+            .navigationTitle("HiMosaic")
             .navigationBarTitleDisplayMode(.large)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Text("HiMosaic")
-                        .font(.largeTitle.bold())
-                        .foregroundColor(.primary)
-                        .padding(.top, 20)
-                }
-                
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        
-                    } label: {
-                        VStack {
-                            Image(systemName: "arrow.triangle.2.circlepath")
-                            
-                            Text("Change Pic")
-                                .font(.caption)
-                        }
-                        .foregroundColor(.blue)
-                        .padding(.top, 20)
-                    }
-                }
-            }
-            .onAppear {
-                service.recognizeText(in: UIImage(named: "fake_info"))
-            }
-            .onChange(of: bName) { newValue in
-                $service.textItems.filter { $item in
-                    item.type == RegexPattern.name
-                }.forEach { $item in
-                    item.validated = newValue
-                }
-            }
-            .onChange(of: bNumber) { newValue in
-                $service.textItems.filter { $item in
-                    item.type == RegexPattern.number
-                }.forEach { $item in
-                    item.validated = newValue
-                }
-            }
-            .onChange(of: bPhoneNumber) { newValue in
-                $service.textItems.filter { $item in
-                    item.type == RegexPattern.phoneNumber
-                }.forEach { $item in
-                    item.validated = newValue
-                }
-            }
-            .onChange(of: bEmail) { newValue in
-                $service.textItems.filter { $item in
-                    item.type == RegexPattern.email
-                }.forEach { $item in
-                    item.validated = newValue
-                }
-            }
-            .onChange(of: bUrl) { newValue in
-                $service.textItems.filter { $item in
-                    item.type == RegexPattern.url
-                }.forEach { $item in
-                    item.validated = newValue
-                }
-            }
-            .onChange(of: bCreditCardNumber) { newValue in
-                $service.textItems.filter { $item in
-                    item.type == RegexPattern.creditCardNumber
-                }.forEach { $item in
-                    item.validated = newValue
-                }
-            }
         }
         .navigationViewStyle(.stack)
     }
---
+++ HiMosaic.swiftpm/View/MosaicOperationView.swift
@@ -0,0 +1,131 @@
+//
+//  MosaicOperationView.swift
+//  
+//
+//  Created by Roy Rao on 2022/4/20.
+//
+
+import SwiftUI
+import Vision
+
+struct MosaicOperationView: View {
+    @ObservedObject private var service: HMService = .shared
+    @State private var bName = false
+    @State private var bNumber = false
+    @State private var bPhoneNumber = false
+    @State private var bEmail = false
+    @State private var bUrl = false
+    @State private var bCreditCardNumber = false
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Image("fake_info")
+                    .resizable()
+                    .scaledToFit()
+                    .overlay {
+                        GeometryReader { geo in
+                            ForEach($service.textItems, id: \.self) { $item in
+                                Rectangle()
+                                    .path(in: VNImageRectForNormalizedRect(
+                                        item.normalizedRect,
+                                        Int(geo.size.width),
+                                        Int(geo.size.height)
+                                    ))
+                                    .fill(item.validated ? Color.gray : .black.opacity(0.0001))
+                                    .onTapGesture {
+                                        item.validated.toggle()
+                                    }
+                            }
+                        }
+                    }
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    Toggle("Names", isOn: $bName)
+                        .toggleStyle(CheckboxToggleStyle(style: .square))
+                        .foregroundColor(.blue)
+                    
+                    Toggle("Numbers", isOn: $bNumber)
+                        .toggleStyle(CheckboxToggleStyle(style: .square))
+                        .foregroundColor(.blue)
+                    
+                    Toggle("Phone Numbers", isOn: $bPhoneNumber)
+                        .toggleStyle(CheckboxToggleStyle(style: .square))
+                        .foregroundColor(.blue)
+                    
+                    Toggle("Emails", isOn: $bEmail)
+                        .toggleStyle(CheckboxToggleStyle(style: .square))
+                        .foregroundColor(.blue)
+                    
+                    Toggle("URLs", isOn: $bUrl)
+                        .toggleStyle(CheckboxToggleStyle(style: .square))
+                        .foregroundColor(.blue)
+                    
+                    Toggle("Credit Card Numbers", isOn: $bCreditCardNumber)
+                        .toggleStyle(CheckboxToggleStyle(style: .square))
+                        .foregroundColor(.blue)
+                }
+            }
+            
+            if service.textItems.isEmpty {
+                ProgressView()
+                    .progressViewStyle(.circular)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                    .background(.ultraThinMaterial)
+            }
+        }
+        .navigationTitle("Workspace")
+        .navigationBarTitleDisplayMode(.large)
+        .onAppear {
+            service.recognizeText(in: UIImage(named: "fake_info"))
+        }
+        .onChange(of: bName) { newValue in
+            $service.textItems.filter { $item in
+                item.type == RegexPattern.name
+            }.forEach { $item in
+                item.validated = newValue
+            }
+        }
+        .onChange(of: bNumber) { newValue in
+            $service.textItems.filter { $item in
+                item.type == RegexPattern.number
+            }.forEach { $item in
+                item.validated = newValue
+            }
+        }
+        .onChange(of: bPhoneNumber) { newValue in
+            $service.textItems.filter { $item in
+                item.type == RegexPattern.phoneNumber
+            }.forEach { $item in
+                item.validated = newValue
+            }
+        }
+        .onChange(of: bEmail) { newValue in
+            $service.textItems.filter { $item in
+                item.type == RegexPattern.email
+            }.forEach { $item in
+                item.validated = newValue
+            }
+        }
+        .onChange(of: bUrl) { newValue in
+            $service.textItems.filter { $item in
+                item.type == RegexPattern.url
+            }.forEach { $item in
+                item.validated = newValue
+            }
+        }
+        .onChange(of: bCreditCardNumber) { newValue in
+            $service.textItems.filter { $item in
+                item.type == RegexPattern.creditCardNumber
+            }.forEach { $item in
+                item.validated = newValue
+            }
+        }
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        MosaicOperationView()
+    }
+}
